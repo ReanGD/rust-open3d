9f274486638e9a885b003517e016385e756fbc8a
ReanGD <reangd@yandex.ru>
2015-10-06 23:24:10 +0300

    mesh теперь работает с разными типами вершин

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,21 +1,29 @@
-use glium::{VertexBuffer, IndexBuffer};
+use glium::{Vertex, VertexBuffer, IndexBuffer};
 use glium::backend::glutin_backend::GlutinFacade;
-use glium::index::{PrimitiveType, BufferCreationError};
+use glium::index::{PrimitiveType};
 
 #[derive(Copy, Clone)]
-pub struct BaseVertex {
-    pub position: [f32; 3],
+pub struct VertexBase {
+    pub pos: [f32; 3],
 }
 
-implement_vertex!(BaseVertex, position);
+implement_vertex!(VertexBase, pos);
 
-pub struct Mesh {
-    pub vb: VertexBuffer<BaseVertex>,
+#[derive(Copy, Clone)]
+pub struct VertexTex {
+    pub pos: [f32; 3],
+    pub tex: [f32; 2],
+}
+
+implement_vertex!(VertexTex, pos, tex);
+
+pub struct Mesh<TVertex> where TVertex: Copy {
+    pub vb: VertexBuffer<TVertex>,
     pub ib: IndexBuffer<u32>,
 }
 
-impl Mesh {
-    pub fn new(facade: &GlutinFacade, vb_data: &[BaseVertex], ib_data: &[u32]) -> Result<Mesh, String>
+impl<TVertex> Mesh<TVertex> where TVertex: Vertex {
+    pub fn new(facade: &GlutinFacade, vb_data: &[TVertex], ib_data: &[u32]) -> Result<Mesh<TVertex>, String>
     {
         let vb = match VertexBuffer::new(facade, vb_data) {
             Ok(vb) => vb,
@@ -24,11 +32,7 @@ impl Mesh {
 
         let ib = match IndexBuffer::new(facade, PrimitiveType::TrianglesList, ib_data) {
             Ok(ib) => ib,
-            Err(e) => return Err(format!("Error create index buffer: {}", match e {
-                BufferCreationError::IndexTypeNotSupported => format!("IndexTypeNotSupported"),
-                BufferCreationError::PrimitiveTypeNotSupported => format!("PrimitiveTypeNotSupported"),
-                BufferCreationError::BufferCreationError(creation_err) => format!("BufferCreationError={}", creation_err),
-            }))
+            Err(e) => return Err(format!("Error create index buffer: {:?}", e))
         };
 
         Ok(Mesh {
