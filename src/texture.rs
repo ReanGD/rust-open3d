3b5da9c46ebb448460221dda1093ca722338f38c
ReanGD <reangd@yandex.ru>
2015-10-09 22:45:26 +0300

    add texture class

---
+++ src/texture.rs
@@ -0,0 +1,31 @@
+use std;
+use utils;
+use image;
+use glium::texture::texture2d::Texture2d;
+use glium::backend::glutin_backend::GlutinFacade;
+
+pub struct Texture {
+    pub data: Texture2d,
+}
+
+impl Texture {
+    pub fn new(facade: &GlutinFacade, filename: &std::path::Path) -> Result<Texture, String> {
+        let texture_path = try!(utils::get_base_dir()).join(filename);
+        println!("load texture: \"{}\"", texture_path.display());
+
+        let image = match image::open(texture_path.as_path()) {
+            Ok(v) => v,
+            Err(e) => return Err(format!("error load texture \"{}\": {}", texture_path.display(), e))
+        };
+
+        let texture = match Texture2d::new(facade, image) {
+            Ok(v) => v,
+            Err(e) => return Err(format!("error create texture \"{}\": {:?}", texture_path.display(), e))
+        };
+
+        Ok(Texture {
+            data: texture,
+        })
+    }
+
+}
